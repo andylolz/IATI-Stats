6b3ccb499f1a1bbd491d353ada6ec34b69ed6a53
Andy Lulham <a.lulham@gmail.com>
2019-08-01 15:22:26 +0100

    Do sentry integration a different way

--- worker.py
+++ worker.py
@@ -2,7 +2,8 @@ import os
 
 import redis
 from rq import Worker, Queue, Connection
-from rq.contrib.sentry import register_sentry
+import sentry_sdk
+from sentry_sdk.integrations.rq import RqIntegration
 
 
 listen = ['high', 'default', 'low']
@@ -11,10 +12,14 @@ redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
 
 conn = redis.from_url(redis_url)
 
-if os.getenv('SENTRY_DSN'):
-    register_sentry(os.getenv('SENTRY_DSN'))
-
 if __name__ == '__main__':
+    SENTRY_DSN = os.getenv('SENTRY_DSN')
+    if SENTRY_DSN:
+        sentry_sdk.init(
+            dsn=SENTRY_DSN,
+            integrations=[RqIntegration()],
+        )
+
     with Connection(conn):
         worker = Worker(map(Queue, listen))
-        worker.work()
+        worker.work(logging_level='WARN')
